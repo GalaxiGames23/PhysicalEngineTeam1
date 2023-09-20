31152bc6adc6bbb81d240c4a8c0b61a1ea707c82
BeaGC <beatrice.garcia-cegarra1@uqac.ca>
2023-09-20 14:18:56 -0400

    Set InverseMass accessors + Add Matrix class

--- src/Particule.h
+++ src/Particule.h
@@ -23,13 +23,13 @@ public:
 	Particule(double invertMass, Vector position, Vector velocity, Vector gravity);
 
 	// Setters
-	void SetMass(double invertMass) { this->invertMass = invertMass; }
+	void SetMass(double mass) { this->invertMass = 1/mass; }
 	void SetPosition(Vector position) { this->position = position; }
 	void SetVelocity(Vector velocity) { this->velocity = velocity; }
 	void SetGravity(Vector gravity) { this->gravity = gravity; }
 
 	// Getters
-	double GetMass() { return this->invertMass; }
+	double GetMass() { return 1/this-> invertMass; }
 	Vector GetPosition() { return this->position; }
 	Vector GetVelocity() { return this->velocity; }
 	Vector GetGravity() { return this->gravity; }
